07f39c5738398736b186e03ca58fc02319fbfdc0
aishatariq1980 <167800892+aishatariq1980@users.noreply.github.com>
2024-04-22 22:53:42 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+//make an array with 5 country name to visit
+let countriesTovisits: string[] = [ "Saudia","Qatar" ,"dubai","Uk","newzealand" ] ;
+
+//print orginal order
+console.log("orginal order:", countriesTovisits) ;
+
+//print country name with alphabatical order without changing the orignal array
+console.log("alphabatical Order:" , [...countriesTovisits] .sort()) ;
+
+//show that the array in its orignal order
+console.log("Still in original order:" , countriesTovisits) ;
